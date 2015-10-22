80757572cb6e979ae4ef4928b26a04c06833c30f
Kristian Poslek <kristian.poslek@comsysto.com>
2015-10-22 12:20:38 +0200

    intial gulp build working

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+var del = require('del');
+var merge = require('merge2');
+var sourcemaps = require('gulp-sourcemaps');
+
+var tsProject = ts.createProject({
+    declaration: true,
+    module: 'commonjs',
+    jsx: 'react',
+    noExternalResolve: false
+});
+
+gulp.task('clean', function (callback) {
+    return del(['build/**']);
+    //callback();
+});
+
+gulp.task('ts', ['clean'], function () {
+    var tsResult = gulp.src(['src/**/*.tsx', 'src/**/*.ts'])
+        .pipe(sourcemaps.init())
+        .pipe(ts(tsProject));
+
+    return merge([
+        tsResult.dts.pipe(sourcemaps.write()).pipe(gulp.dest('build/typings')),
+        tsResult.js.pipe(sourcemaps.write()).pipe(gulp.dest('build/js'))
+    ]);
+});
+
+gulp.task('js', ['ts'], function () {
+    return browserify({
+        entries: './build/js/index.js',
+        debug: true
+    })
+        .bundle()
+        .on('error', function (e) {
+            console.log('Error: ' + e.toString());
+            this.emit('end');
+        })
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest('./build/compiled'))
+});
+
+gulp.task('watch', ['ts', 'js'], function () {
+    gulp.watch(['src/**/*.tsx', 'src/**/*.ts'], ['ts', 'js']);
+});
+
+gulp.task('default', ['ts', 'js']);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+///<reference path="../typings/tsd.d.ts"/>
+import * as React from "react";
+
+class BitchComponent extends React.Component<any, any> {
+    constructor(props) {
+        super(props);
+        console.log('blaaa');
+    }
+}
